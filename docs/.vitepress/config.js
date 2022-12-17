fa6d12bdf6e027d35140de5923769bbad754ec88
hongjyoun <hongjyoun@gmail.com>
2022-12-17 10:22:42 +0900

    ga script update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -17,6 +17,8 @@ export default {
       ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
       ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
       ['link', { href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap', rel: 'stylesheet' }],
+      ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-GV27QZ3Z73' }],
+      ['script', {}, "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-GV27QZ3Z73');"],
       
       // google site verification
       ['meta', { name: 'google-site-verification', content: 'UQi6-4mGgJgr2v4COjlqZsGVxGumi6GYlv2P0_qnikk' }],
